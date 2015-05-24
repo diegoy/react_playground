ffce3a67c4664900ad8641bac3d65717e0db0dce
Diego Y. K <diegoy@gmail.com>
2015-05-24 20:29:42 -0300

    Adds amounts idicators

--- app/assets/javascripts/components/records.js.jsx
+++ app/assets/javascripts/components/records.js.jsx
@@ -8,12 +8,32 @@ this.Records = React.createClass({
   addRecord: function(record) {
     records = this.state.records.slice();
     records.push(record);
-    this.setState({records: records})
+    this.setState({records: records});
+  },
+  credits: function() {
+    var credits = this.state.records.filter(function(val) { return val.amount >=0 });
+    return credits.reduce(function(prev, curr) {
+      return prev + parseFloat(curr.amount);
+    }, 0);
+  },
+  debits: function() {
+    debits = this.state.records.filter(function(val) { return val.amount < 0 });
+    return debits.reduce(function(prev, curr) {
+      return prev + parseFloat(curr.amount);
+    }, 0);
+  },
+  balance: function() {
+    return this.debits() + this.credits()
   },
   render: function() {
     return <div className="records">
              <h2 className="title"> Records </h2>
-             <RecordForm handleNewRecord={this.addRecord}/>
+             <div className="row">
+              <AmountBox type="success" amount={this.credits()} text="Credit"/>
+              <AmountBox type="danger" amount={this.debits()} text="Debit"/>
+              <AmountBox type="info" amount={this.balance()} text="Balance"/>
+              <RecordForm handleNewRecord={this.addRecord}/>
+             </div>
              <hr/>
              <table className="table table-bordered">
                 <thead>
